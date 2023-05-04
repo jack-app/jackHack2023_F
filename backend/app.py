21a3451ccfece1ec3943e145a23aa6c49f5404e2
itoxu110 <yitenghuixi@gmail.com>
2023-05-04 10:49:38 +0900

    comment返す関数の追加

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
   return 'Hello World'
 
+@app.route('/text/<text_id>', methods=["GET"])
+def get_text(text_id):
+  #text_idとlist参照してコメントと点数返したい
+  return '%sを参照する' % text_id
+
 if __name__ == '__main__':
   app.debug = True
   app.run(host='127.0.0.1', port=5000)
---
+++ backend/matcing.py
@@ -0,0 +1,12 @@
+def matchingPoint(score):
+    score += 10
+    return score
+
+def comment(score):
+    #点数に応じて文字列返す
+    if score <= a :
+        return "string1"
+    elif a < score < b:
+        return "string2"
+    elif b <= score:
+        return "string3"
