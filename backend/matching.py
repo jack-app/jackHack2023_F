b6a84a4b303aa508973a86acaae5f2d043e91f67
itoxu110 <yitenghuixi@gmail.com>
2023-05-05 14:34:06 +0900

    コンフリクト解消

--- backend/matching.py
+++ backend/matching.py
@@ -7,7 +7,7 @@ def ratingComment(score):
     #点数に応じて文字列返す
     if score <= 20 :
         return "もう！ふんずけるわよあんた！毎日ちゃんと風呂入っとんの？！"
-    elif 20 < score < 50:
+    elif 20 < score < 60:
         return "うーん、微妙だわねえ、今後に期待かしら"
-    elif 50 <= score:
+    elif 60 <= score:
         return "なかなかやればできるじゃない。あなたは磨きがいがある子猫ちゃんよ！"
